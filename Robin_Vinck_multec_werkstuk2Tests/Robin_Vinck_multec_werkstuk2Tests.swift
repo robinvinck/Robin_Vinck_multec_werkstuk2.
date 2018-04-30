121183b965ae4a03b92ac6cd19ead92b01e366dc
Robin Vinck <robin.vinck@student.ehb.be>
2018-04-30 10:13:25 +0200

    test

---
+++ Robin_Vinck_multec_werkstuk2Tests/Robin_Vinck_multec_werkstuk2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Robin_Vinck_multec_werkstuk2Tests.swift
+//  Robin_Vinck_multec_werkstuk2Tests
+//
+//  Created by Robin Vinck on 30/04/18.
+//  Copyright © 2018 Robin Vinck. All rights reserved.
+//
+
+import XCTest
+@testable import Robin_Vinck_multec_werkstuk2
+
+class Robin_Vinck_multec_werkstuk2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
